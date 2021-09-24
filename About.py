4bcaaaa60411fb8abc53136779f217c7f7c90fe7
hammadsatti16 <91092355+hammadsatti16@users.noreply.github.com>
2021-09-24 19:07:04 +0500

    Update About.py

--- About.py
+++ About.py
@@ -10,9 +10,6 @@ import pandas as pd
 from keras.models import load_model
 # @st.cache
 def app():
-    st.markdown("# About")
-    model = load_model("C:/Users/hamma/Downloads/Medina/pages/Translator.h5")   
-    x=model.summary()
     st.markdown("#### This webiste is translates the english sentence to french sentence the RNN based LSTM encoding decoding model is used.")
     
     
